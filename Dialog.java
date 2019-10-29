a0712321125a92b3ce614fe0e13b47f7d8e6cc92
YuraVolk <42916748+YuraVolk@users.noreply.github.com>
2019-10-29 18:08:01 +0300

    Update Dialog.java

--- Dialog.java
+++ Dialog.java
@@ -6,10 +6,6 @@ import java.util.*;
  * Client Id: a7bf6e4aeec14099bedf9d0e4fc9f351
  * Client Secret: d6f13f9fc62e49b781d63a02ebcf51c2
  * Redirect URI: https://music.panel.authorize.com
- * Authorize link:
- * https://accounts.spotify.com/authorize?client_id=a7bf6e4aeec14099bedf9d0e4fc9f351&redirect_uri=https://music.panel.authorize.com&response_type=code
- * Acess Token:
- * AQBIlhTkz0fOnfRRwCJQN-1O6Zh-B25vpQe18KTZV2luunGuI-FeDzH-00kOpa47Y_2MjZxa5MmvgRdLrUFwIFzdQ64WHECnCKMLjTwPb9H_cjC8h0w4r2WKSxNzcnp5XwNfsVAhfCpuJF_dSx9YWcrGxz0_pHMqkiMX1s9NtZIyomBz9O9TvPxGU1eH-52MNQxYZLCcov8B
  */
 
 class Dialog {
@@ -17,93 +13,54 @@ class Dialog {
     private final Scanner scanner = new Scanner(System.in);
 
 
-
-    private ArrayList<String> generateArrayList(String... val) { //TODO Replace with model call
-        return new ArrayList<>(Arrays.asList(val));
-    }
-
-    private void printReleases(ArrayList<String> releases) {
-        releases.forEach(System.out :: println);
-    }
-
-    private void printReleases(Map<String, ArrayList<String>> releases) {
-        releases.forEach((key, value) -> System.out.println(key + " " + value));
-    }
-
-    private void getNewReleases() {
-        System.out.println("---NEW RELEASES---");
-        Map<String, ArrayList<String>> releases =  new HashMap<>();
-        releases.put("Mountains", generateArrayList("Sia", "Diplo", "Labrinth"));
-        releases.put("Runaway", generateArrayList("Lil Peep"));
-        releases.put("The Greatest Show", generateArrayList("Panic! At The Disco"));
-        releases.put("All Out Life", generateArrayList("Slipknot"));
-        printReleases(releases);
-    }
-
-    private void getFeatured() {
-        System.out.println("---FEATURED---");
-        ArrayList<String> playlists = new ArrayList<>();
-        playlists.add("Mellow Morning");
-        playlists.add("Wake Up and Smell the Coffee");
-        playlists.add("Monday Motivation");
-        playlists.add("Songs to Sing in the Shower");
-        printReleases(playlists);
-    }
-
-    private void getCategories() {
-        System.out.println("---CATEGORIES---");
-        ArrayList<String> playlists = new ArrayList<>();
-        playlists.add("Top lists");
-        playlists.add("Pop");
-        playlists.add("Mood");
-        playlists.add("Latin");
-        printReleases(playlists);
-    }
-
-    private void getPlaylistReleases(String category) {
-        System.out.printf("---%S PLAYLISTS---\n", category);
-        //TODO Call controller. Controller will call model
-        ArrayList<String> playlists = new ArrayList<>();
-        playlists.add("Walk Like A Badass");
-        playlists.add("Rage Beats");
-        playlists.add("Arab Mood Booster");
-        playlists.add("Sunday Stroll");
-        printReleases(playlists);
-    }
-
     void startProgram() {
         final String clientId = "a7bf6e4aeec14099bedf9d0e4fc9f351";
         final String redirectURI = "https://music.panel.authorize.com";
+        final String secret = "d6f13f9fc62e49b781d63a02ebcf51c2";
+
         boolean isSigned = false;
+
+        Server server = new Server(redirectURI, clientId, secret);
 
         while(!choice.equals("exit")) {
             choice = scanner.next();
+
             if (choice.equals("auth")) {
-                System.out.printf("https://accounts.spotify.com/authorize?client_id=%s" +
-                        "&redirect_uri=%s&response_type=code", clientId, redirectURI);
-                isSigned = true;
-                System.out.println("---SUCCESS---");
+                System.out.println("making http request for access_token...");
+                isSigned = server.requestAccessToken();
+                if (isSigned == false) {
+                    System.out.println("Internal server error. Please reload the app.");
+                } else {
+                    isSigned = true;
+                    System.out.printf("{\"access_token\": \"%s\", \"scope\": \"\"}\n",
+                            server.getAccessToken());
+                    System.out.println("---SUCCESS---");
+                }
+
             }
+
             if (!isSigned) {
                 System.out.println("Please, provide access for application.");
                 continue;
             }
+
             switch (choice) {
                 case "new":
-                    getNewReleases();
+                    server.getNew();
                     break;
                 case "featured":
-                    getFeatured();
+                    server.getFeatured();
                     break;
                 case "categories":
-                    getCategories();
+                    server.getCategories();
                     break;
                 case "playlists":
-                    getPlaylistReleases(scanner.next());
+                    server.getCategory(scanner.next());
             }
         }
 
         System.out.println("---GOODBYE!---");
     }
 }
+
 
